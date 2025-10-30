23609d2aa3416fea4fcf7d31c3d6b601cc2b991f
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-30 01:58:59 +0100

    Update api.php
    inicio listado naves sin piloto

--- Estarword/routes/api.php
+++ Estarword/routes/api.php
@@ -20,6 +20,7 @@ Route::apiResource('mantenimientos', MantenimientoController::class);
 
 Route::post('/naves/asignar-piloto', [NaveController::class, 'asignarPiloto']);
 Route::post('/naves/desasignar-piloto', [NaveController::class, 'desasignarPiloto']);
+Route::get('/naves/inactivas')
 /*
 Route::get('/naves', [NaveController::class, 'index']);
 Route::get('/naves/{id}', [NaveController::class, 'show']);
